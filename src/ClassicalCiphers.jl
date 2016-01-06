f9777201559d48cd5bd5e38f734001ff309847a0
Smaug123 <patrick@patrickstevens.co.uk>
2016-01-06 16:05:36 +0000

    Add Portas cipher encryption/decryption

--- src/ClassicalCiphers.jl
+++ src/ClassicalCiphers.jl
@@ -7,10 +7,12 @@ include("monoalphabetic.jl")
 include("caesar.jl")
 include("vigenere.jl")
 include("solitaire.jl")
+include("portas.jl")
 
 export encrypt_monoalphabetic, decrypt_monoalphabetic, crack_monoalphabetic,
        encrypt_caesar, decrypt_caesar, crack_caesar,
        encrypt_vigenere, decrypt_vigenere, crack_vigenere,
+       encrypt_portas, decrypt_portas,
        encrypt_solitaire, decrypt_solitaire,
        string_fitness, index_of_coincidence
 
